0cd822ec18fab26f9366632ecfa0779bf3b9a063
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-19 22:57:01 +0530

    ADDED: Program to convert emp pay roll to client server architecture

---
+++ Js/SiteProperties.js
@@ -0,0 +1,6 @@
+let site_properties = {
+    use_local_storage:"true",
+    home_page: "C:\Users\Lavanya\Desktop\Client Server Architecture\Pages\home.html",
+    add_emp_payroll_page:  "C:\Users\HP\OneDrive\Desktop\clientserver\Pages\payrollform.html",
+    server_url:"http://127.0.0.1:3000/EmployeePayrollDB/"
+};
---
+++ Js/home.js
@@ -0,0 +1,56 @@
+let empPayrollList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = 
+    empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+});
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? 
+            JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
+//Template literal ES6 feature
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if(empPayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for ( const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${stringyfyDate(empPayrollData._startDate)}</td>
+            <td>
+                <img id="${empPayrollData.id}" onclick="remove(this)" alt="delete" src="file:///C:/Users/HP/OneDrive/Desktop/clientserver/Assets/icons/delete-black-18dp.svg">
+                <img id="${empPayrollData.id}" onclick="update(this)" alt="edit" src="file:///C:/Users/HP/OneDrive/Desktop/clientserver/Assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+    `;
+    }
+        document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {                 
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData.id == node.id);
+    if (!empPayrollData) return;
+    const index = empPayrollList.map(empData => empData.id).indexOf(empPayrollData.id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+}
